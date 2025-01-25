f0fba9d1ad33b3a28e499b97283db2bb71b57011
HrutvikKhatkar <hrutvikkhatkar25@gmail.com>
2025-01-25 19:18:31 +0530

    uri set

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,14 @@ const blogRoutes = require("./routes/blogs");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/";
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
